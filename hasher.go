138778795e823751ddafdb12ea55d4c989cb60aa
JeremyMWells <jmw.jeremymwells@gmail.com>
2019-10-12 07:44:13 -0400

    changed return

--- hasher.go
+++ hasher.go
@@ -3,14 +3,14 @@ package hasher
 import "crypto/md5"
 import "crypto/sha1"
 
-func Md5(bytes []byte) [32]byte {
+func Md5(bytes []byte) [16]byte {
 	return md5.Sum(bytes)
 }
 
-func Sha1(bytes []byte) [40]byte {
+func Sha1(bytes []byte) [20]byte {
 	return sha1.Sum(bytes)
 }
 
-func Hash(bytes []byte) ([40]byte, [32]byte) {
+func Hash(bytes []byte) ([20]byte, [16]byte) {
 	return Sha1(bytes), Md5(bytes)
 }
